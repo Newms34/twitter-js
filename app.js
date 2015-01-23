ed93f62602d90d7e2839e8cfa016a9acd33aced3
crullian <crullian@gmail.com>
2015-01-23 15:36:23 -0500

    Small change to text

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 var express = require('express'),
-morgan = require('morgan');
+  morgan = require('morgan');
 
-var app=express();
+var app = express();
 
 app.use(morgan('dev'));
 
-app.get('/',function (req,res) {
-	console.log('A log');
-	res.send('Go away! You\'re not welcome here.');
+app.get('/', function(req, res) {
+  console.log('A big log');
+  res.send('Go away! You\'re not welcome here.');
 });
 
-app.get('/tweets',function (req,res) {
-	res.send('Is there a bird in here?');
+app.get('/tweets', function(req, res) {
+  res.send('Is there a bird in here?');
 });
 
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function() {
 
 });
